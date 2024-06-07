dec9a75de5398854defd38594ab9ce4bfe717726
Jai0212 <jj.joshijai@gmail.com>
2024-06-07 17:04:11 +0400

    changes

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ import 'dotenv/config'
 
 // app config
 const app = express()
-const port = 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "https://chore-hero.netlify.app/"
+}))
 
 // db connection
 connectDB();
@@ -29,8 +30,8 @@ app.get("/", (req, res) => {
     res.send("Working")
 })
 
+const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
-
-// mongodb+srv://jaijoshi:820GQPOYG@cluster0.wjlep0g.mongodb.net/?
