10fe565f7fcf6cac8496135638b6f6d44ac0f939
Jai0212 <jj.joshijai@gmail.com>
2024-06-09 17:17:04 +0400

    changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://jaijoshi:820GQPOYG@cluster0.wjlep0g.mongodb.net/chore_hero').then(() => console.log("DB Conected"))
+    await mongoose.connect('mongodb+srv://jaijoshi:820GQPOYG@cluster0.wjlep0g.mongodb.net/chore_hero').then(() => console.log("DB Connected"))
 }
